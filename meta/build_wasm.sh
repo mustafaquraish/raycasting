593aa177a926e071f4622c4c3db6e4ba2326d168
Mustafa Quraish <mustafaq9@gmail.com>
2024-05-20 00:08:47 -0400

    update HTML + optimize emcc build

--- meta/build_wasm.sh
+++ meta/build_wasm.sh
@@ -14,10 +14,12 @@ fi
 
 cp assets/index.html build/
 emcc $FILE \
+    -O3 \
     -DOC_NO_BACKTRACE \
     -s USE_SDL=2 \
     -s USE_ZLIB=1 \
     -s ALLOW_MEMORY_GROWTH=1 \
     --preload-file assets \
     --use-preload-plugins \
-    -o build/index.js
+    -o build/index.js \
+    -s DISABLE_DEPRECATED_FIND_EVENT_TARGET_BEHAVIOR=0
